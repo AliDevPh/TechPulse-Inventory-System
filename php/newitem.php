622d268ddebc2dbdf9bec1e28d6405066a90a32e
Kyell Dimatatac <Ultara567@gmail.com>
2024-02-18 19:30:48 +0800

    Update newitem.php

--- php/newitem.php
+++ php/newitem.php
@@ -16,7 +16,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
 
     if (mysqli_num_rows ($result) > 0) {
-        header('Location: ../newitem.html?error=duplicate_product_id');
+        header('Location: ../newitem.html?error=duplicate_product');
         exit();
     } else {
         $sql = "INSERT INTO inventory (`ProdName`, `ProdID`, `Stock`, `Price ($)`) VALUES (?, ?, ?, ?)";
